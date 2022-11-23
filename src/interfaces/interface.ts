43318ae7b47a506e5d9322ddf684698dfefb2e63
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-22 18:48:54 -0800

    Create interfaces

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const SET_IS_STARTED = 'SET_IS_STARTED';
+export const SET_RESULT = 'SET_RESULT';
+
+export type SetState= 'SET_IS_STARTED' | 'SET_RESULT';
+export type Result = undefined | 'success' | 'fail';
+
+export interface State {
+  isStarted: boolean,
+  result: Result,
+}
+
+export interface Action {
+  type: SetState,
+  result?: Result,
+}
+
+export interface ButtonProps {
+  isStarted: boolean,
+  setIsStarted: React.Dispatch<Action>,
+}
