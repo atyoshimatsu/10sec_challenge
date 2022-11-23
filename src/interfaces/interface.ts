f279721631a1800ebf5a10d0e278b52db09cc4b3
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-11-22 20:17:46 -0800

    Add time to state

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -2,18 +2,25 @@ import React from "react";
 
 export const SET_IS_STARTED = 'SET_IS_STARTED';
 export const SET_RESULT = 'SET_RESULT';
+export const SET_TIMER = 'SET_TIMER';
 
-export type SetState= 'SET_IS_STARTED' | 'SET_RESULT';
+export type SetState= 'SET_IS_STARTED' | 'SET_RESULT' | 'SET_TIMER';
 export type Result = undefined | 'success' | 'fail';
 
 export interface State {
   isStarted: boolean,
   result: Result,
+  time?: string,
 }
 
 export interface Action {
   type: SetState,
   result?: Result,
+  time?: string
+}
+
+export interface ClockProps {
+  time: string | undefined,
 }
 
 export interface ButtonProps {
