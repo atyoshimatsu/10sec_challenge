362f3d8fef58c69dd3f71b8b10f90db46a7d1791
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-06-02 22:51:23 -0700

    Modify clock time component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -3,9 +3,20 @@ import { ClockProps } from '../interfaces/interface';
 
 const Clock = (props: ClockProps):JSX.Element => {
   const { time } = props;
+  const time1 = !!time ? time.split('')[0] : '0';
+  const time2 = !!time ? time.split('')[1] : '0';
+  const time3 = !!time ? time.split('')[3] : '0';
+  const time4 = !!time ? time.split('')[4] : '0';
+
   return (
     <div className='clock-time'>
-      {time ?? '00.00'}
+      <div className='clock-time'>
+        <div>{time1}</div><div>{time2}</div>
+      </div>
+      <div>.</div>
+      <div className='clock-time'>
+        <div>{time3}</div><div>{time4}</div>
+      </div>
     </div>
   );
 }
