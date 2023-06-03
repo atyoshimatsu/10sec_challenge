a3150424360949f1152b4d4528cbb679e3cba247
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-06-02 17:37:46 -0700

    Add the result state to the reducer

--- src/hooks/useApplicationData.ts
+++ src/hooks/useApplicationData.ts
@@ -1,59 +1,68 @@
 import { useEffect, useReducer } from 'react'
-import { State, Action, SET_IS_STARTED, SET_RESULT, SET_TIME, Result } from "../interfaces/interface";
+import { State, Action, SET_STATE, Result } from "../interfaces/interface";
 
 const useApplicationData = () => {
   const reducer = (state: State, action: Action): State => {
-    if (action.type === SET_RESULT && !action.result) {
-      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
-    }
-
     switch (action.type) {
-      case SET_IS_STARTED:
+      case SET_STATE.IS_STARTED:
         return {
           ...state,
           isStarted: !state.isStarted,
+          result: undefined,
           startTime: Date.now(),
         }
-      case SET_RESULT:
+      case SET_STATE.RESULT:
         return {
           ...state,
           result: action.result,
         }
-      case SET_TIME:
+      case SET_STATE.TIME:
         return {
           ...state,
           time: action.time,
         }
+      default:
+        throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
     }
   }
 
-  const setIsStarted = () => dispatch({ type: SET_IS_STARTED });
-  const setResult = (result: Result) => {
-    return dispatch({ type: SET_RESULT, result });
-  }
+  const setIsStarted = () => dispatch({ type: SET_STATE.IS_STARTED });
 
   const [state, dispatch] = useReducer(reducer,{
     isStarted: false,
     result: undefined,
-    time: '00.00',
+    time: undefined,
     startTime: Date.now(),
   });
 
   useEffect(() => {
     const timer = setInterval(() => {
       const timeDiff = Date.now() - state.startTime;
-      const time =new Date(timeDiff).toISOString().slice(17, 22);
-      const setTime = () => dispatch({ type: SET_TIME, time });
+      const time = new Date(timeDiff).toISOString().slice(17, 22);
+      const setTime = () => dispatch({ type: SET_STATE.TIME, time });
       if(state.isStarted) setTime();
     }, 10);
 
+    const setResult = () => {
+      let result: Result = undefined;
+      if (state.isStarted || !state.time) {
+        return dispatch({ type: SET_STATE.RESULT, result });
+      }
+
+      if (!state.isStarted && Number(state.time) > 9.94 && Number(state.time) < 10.06) {
+        result = 'success';
+      } else {
+        result = 'fail';
+      }
+      return dispatch({ type: SET_STATE.RESULT, result });
+    }
+    setResult();
     return () => clearInterval(timer);
-  }, [state.isStarted, state.startTime]);
+  }, [state.isStarted, state.startTime, state.time]);
 
   return {
     state,
     setIsStarted,
-    setResult,
   }
 }
 
