25ddc1237ff26a4d64b06c8fc9617f79eeb4d0c2
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-06-02 17:38:09 -0700

    Add the result component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Clock from './components/Clock';
 import Button from './components/Button';
+import Result from './components/Result';
 import useApplicationData from './hooks/useApplicationData';
 
 function App(): JSX.Element {
@@ -11,6 +12,7 @@ function App(): JSX.Element {
 			<div className='clock'>
 				<Clock time={state.time} />
 				<Button isStarted={state.isStarted} setIsStarted={setIsStarted}/>
+				<Result result={state.result} />
 			</div>
     </>
   );
