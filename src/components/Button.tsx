091528e0384d0349f7c8440a464d5d3edc1d20b3
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-06-02 17:39:15 -0700

    Modify set_state in the button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { ButtonProps, SET_IS_STARTED } from '../interfaces/interface';
+import { ButtonProps, SET_STATE } from '../interfaces/interface';
 
 const Button = (props: ButtonProps):JSX.Element => {
   const { isStarted, setIsStarted } = props;
   return (
     <button
       className='clock-button'
-      onMouseDown={() => setIsStarted({type: SET_IS_STARTED})}
+      onMouseDown={() => setIsStarted({ type: SET_STATE.IS_STARTED })}
     >
       {isStarted ? 'Stop' : 'Start'}
     </button>
