521e695502737c64c6a81e6b8202ccf83fda9f24
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-06-03 19:56:52 -0700

    Add confetti component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Clock from './components/Clock';
 import Button from './components/Button';
 import Result from './components/Result';
 import useApplicationData from './hooks/useApplicationData';
+import Confetti from 'react-confetti'
 
 function App(): JSX.Element {
 	const { state, setIsStarted } = useApplicationData();
   return (
     <>
+			{state.result === 'SUCCESS' ? <Confetti /> : null}
 			<div className='clock'>
 				<Clock time={state.time} />
 				<Button isStarted={state.isStarted} setIsStarted={setIsStarted}/>
